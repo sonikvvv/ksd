cb62c4a6c6ebd5078989130198272d3a4c65dfa6
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2022-06-28 18:50:11 +0300

    changed axios settings

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-});
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default axios;
