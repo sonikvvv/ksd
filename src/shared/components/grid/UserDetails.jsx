573f4599f470f2f7b543f77a29d0400bb9eeb558
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2022-07-08 18:38:16 +0300

    Small changes.

--- src/shared/components/grid/UserDetails.jsx
+++ src/shared/components/grid/UserDetails.jsx
@@ -4,49 +4,92 @@ import moment from 'moment';
 
 import LabeledBlock from '../containers/LabeledBlock';
 
-const GridItem = (primary, secondary) => {
-    return (
-        <Grid container>
-            <Grid item xs={12} sm={5}>
-                <Typography
-                    variant="body1"
-                    fontWeight={500}
-                    sx={{ wordWrap: 'break-word' }}
-                >
-                    {primary}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} sm={7}>
-                <Typography
-                    variant="body1"
-                    color="text.secondary"
-                    sx={{ wordWrap: 'break-word' }}
-                >
-                    {secondary}
-                </Typography>
-            </Grid>
-        </Grid>
-    );
-};
-
 const UserDetails = ({ user }) => {
-    const {
-        username = '',
-        phone = '',
-        email = '',
-        dateOfBirth = new Date(),
-    } = user;
-
-    const parsedDateOfBirth = moment(dateOfBirth).format('D MMM YYYY');
-
     return (
         <LabeledBlock title="Basic Details" divider contentPadding={{ p: 3 }}>
             <Stack divider={<Divider flexItem />} spacing={2}>
-                {username !== '' && GridItem('Username', username)}
-                {phone !== '' && GridItem('Phone', phone)}
-                {email !== '' && GridItem('Email', email)}
-                {dateOfBirth !== '' &&
-                    GridItem('Date of birth', parsedDateOfBirth)}
+                <Grid container>
+                    <Grid item xs={12} sm={5}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={500}
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            Username
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <Typography
+                            variant="body1"
+                            color="text.secondary"
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            {user?.username || '-'}
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12} sm={5}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={500}
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            Phone
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <Typography
+                            variant="body1"
+                            color="text.secondary"
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            {user?.phone || '-'}
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12} sm={5}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={500}
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            Email
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <Typography
+                            variant="body1"
+                            color="text.secondary"
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            {user?.email || '-'}
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12} sm={5}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={500}
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            Date of birth
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <Typography
+                            variant="body1"
+                            color="text.secondary"
+                            sx={{ wordWrap: 'break-word' }}
+                        >
+                            {user?.dateOfBirth
+                                ? moment(user?.dateOfBirth).format('D MMM YYYY')
+                                : '-'}
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Stack>
         </LabeledBlock>
     );
